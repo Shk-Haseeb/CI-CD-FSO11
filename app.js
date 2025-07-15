22d17024046b7357822c46230f06e4c01ce1acee
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-15 17:49:30 +0300

    Fixed routing for /version

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ import process from 'process'
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 app.get('/version', (req, res) => {
   res.send('2')
 })
+
+app.use(express.static('dist'))
+
+
 
 app.listen(PORT, () => {
 })
